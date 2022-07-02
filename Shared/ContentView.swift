686d164c527270890d2b2e74d3f9dc06da00f27d
yuki819 <yuki819@mba2018.local>
2022-07-03 02:54:38 +0900

    feature(HelloWorld):　プレビューは iPhone 11 Pro で起動するように指定してみる

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -31,6 +31,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 11 Pro")
+        }
     }
 }
