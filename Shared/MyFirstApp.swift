3e67e6670f1d6dc37e274e70e695d490832ecfbd
yuki819 <yuki819@mba2018.local>
2022-07-03 00:40:55 +0900

    Initial Commit

---
+++ Shared/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  Shared
+//
+//  Created by 小笠原佑樹 on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
